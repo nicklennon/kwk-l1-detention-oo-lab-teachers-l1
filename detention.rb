86a4f06e6018946a7f15eda5705618f5daa984e6
nicklennon <nicklennon.53@gmail.com>
2018-06-24 17:15:22 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,18 @@
 #Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher
+  attr_reader :activity, :students
+  
+  def initialize
+    @activity = "collective punishment"
+    @students = []
+  end
+  
+  def add_student(name)
+    @students << name
+  end
+  
+  def remove_student(name)
+    @students.delete(name)
+  end
+end
